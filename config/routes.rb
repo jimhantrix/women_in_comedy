bb6da7eb30945dc73f57fa2506a5db728fb32ebd
sagar-miglani <smiglani1994@gmail.com>
2017-06-19 20:52:20 -0700

    controller and views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'hello_world', to: 'hello_world#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
