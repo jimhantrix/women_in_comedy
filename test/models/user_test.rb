d8e4350a10b8e1029c452e065cf273a42052cb79
Demario Marbray <toosexybuff500@gmail.com>
2017-06-21 18:21:25 -0700

    rspec test pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
-require 'test_helper'
+require "rails_helper"
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+# RSpec.describe Post, :type => :model do
+#   it "create post" do
+#     user = Post.create!(user_id:1,title: "testing", text: "to day was a good day")
+#   end
+  
+#   describe " creation" do
+#   	it "should hove one item created " do
+#   		expect(Post.all.count).to eq(1)
+#   	end
+# 	end
+# end
+
+
+
+	RSpec.describe User, type: :model do
+
+		before(:each) do
+		 @user = User.create(email: "bobbybobby@gmail.com", password: "password")
+		end
+
+		describe "creations" do
+			it "should have one item created"
+			expect(User.all.count).to eq(1)
+			
+		end
+	end
 end
